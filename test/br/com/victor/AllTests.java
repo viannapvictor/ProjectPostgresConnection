805436b18abf901981b0a126eb828c0f2cd7b45c
Victor Vianna <vitorvianna27@gmail.com>
2023-10-27 18:18:23 -0300

    :test_tube:test: All tests

---
+++ test/br/com/victor/AllTests.java
@@ -0,0 +1,11 @@
+package br.com.victor;
+
+import br.com.victor.cliente.ClienteTeste;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ClienteTeste.class})
+public class AllTests {
+}
