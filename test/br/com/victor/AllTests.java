fae6c2ffc55940dd1382ef66e5cb645e2c2cd6aa
Victor Vianna <vitorvianna27@gmail.com>
2023-10-28 17:00:23 -0300

    :test_tube:test: Product implementation

--- test/br/com/victor/AllTests.java
+++ test/br/com/victor/AllTests.java
@@ -1,11 +1,12 @@
 package br.com.victor;
 
 import br.com.victor.cliente.ClienteTeste;
+import br.com.victor.produto.ProdutoTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ClienteTeste.class})
+@Suite.SuiteClasses({ClienteTeste.class, ProdutoTest.class})
 public class AllTests {
 }
